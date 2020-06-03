f111d16696677969ed189404cf6113a0fa3fda56
Stephan <stephan.kop@outlook.com>
2020-06-03 16:48:03 +0200

    added new content till simons tweet

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,44 @@
 window.onscroll = function() {scrollAnimation()};
 function scrollAnimation() {
     let scroll = document.scrollingElement.scrollTop;
-    if (scroll >= 2000) {
-        console.log('2000');
+    if (scroll >= 9000) {
+        console.log('9000');
     }
-    else if (scroll >= 1900) {
-        console.log('1900');
+    else if (scroll >= 4200) {
+        document.querySelector(".demonstratie-container").classList.remove("opacity-0");
+        document.querySelector(".blm-container").classList.add("opacity-0");
+        console.log('3700');
+    }
+    else if (scroll >= 3300) {
+        document.querySelector(".blm-container").classList.remove("opacity-0");
+        document.querySelector(".optelsom-container").classList.add("opacity-0");
+        console.log('3300');
+    }
+    else if (scroll >= 2800) {
+        document.querySelector(".riot").classList.add("opacity-0");
+        document.querySelector(".optelsom-container").classList.remove("opacity-0");
+        console.log('2800');
+    }
+    else if (scroll >= 2600) {
+        document.querySelector("#riot-img").src = "assets/img/riot3.jpg";
+        console.log('2600');
+    }
+    else if (scroll >= 2300) {
+        document.querySelector("#riot-img").src = "assets/img/riot2.jpg";
+        console.log('2300');
     }
     else if (scroll >= 1800) {
+        document.querySelector(".riot").classList.remove("opacity-0");
+        document.querySelector("#riot-img").src = "assets/img/riot.jpg";
         console.log('1800');
     }
-    else if (scroll >= 1700) {
-        console.log('1700');
-    }
     else if (scroll >= 1600) {
+        document.querySelector(".p-1").classList.add("opacity-0");
         console.log('1600');
     }
-    else if (scroll >= 1500) {
-        console.log('1500');
-    }
-    else if (scroll >= 1400) {
-        console.log('1400');
-    }
-    else if (scroll >= 1300) {
-        console.log('1300');
-    }
-    else if (scroll >= 1200) {
-        console.log('1200');
-    }
-    else if (scroll >= 1100) {
-        console.log('1100');
+    else if (scroll >= 1550) {
+        document.querySelector(".p-1").classList.remove("opacity-0");
+        console.log('1550');
     }
     else if (scroll >= 1050) {
         document.getElementById('red').classList.add("red");
@@ -53,3 +62,12 @@ function scrollAnimation() {
 
     }
 }
+
+window.onload = function() { document.getElementById("blm-logo").addEventListener("click", blmLogo);
+function blmLogo() {
+    document.querySelector(".blm-text-1").classList.add("visible-text");
+    document.querySelector(".blm-text-2").classList.add("visible-text");
+    document.querySelector(".blm-text-3").classList.add("visible-text");
+    document.querySelector(".blm-text-4").classList.add("visible-text");
+    console.log('click');
+}};
