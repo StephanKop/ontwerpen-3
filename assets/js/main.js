377cf846e558a6f998ea40077754458cb7aa3752
Stephan <stephan.kop@outlook.com>
2020-06-07 17:47:49 +0200

    added hamburger menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,6 @@ window.onscroll = function() {scrollAnimation()};
 function scrollAnimation() {
     let scroll = document.scrollingElement.scrollTop;
     let viewportHeight = window.innerHeight;
-    // console.log(viewportHeight);
     if (scroll >= 9000) {
         console.log('9000');
     }
@@ -11,27 +10,28 @@ function scrollAnimation() {
         document.querySelector(".blm-container").classList.add("opacity-0");
         console.log(viewportHeight * 5.2);
     }
-    else if (scroll >= viewportHeight * 4) {
+    else if (scroll >= viewportHeight * 4.5) {
         document.querySelector(".blm-container").classList.remove("opacity-0");
         document.querySelector(".optelsom-container").classList.add("opacity-0");
-        console.log(viewportHeight * 4);
+        console.log("optelsom" + viewportHeight * 4.5);
     }
-    else if (scroll >= viewportHeight * 3.5) {
+    else if (scroll >= viewportHeight * 3.3) {
         document.querySelector(".riot").classList.add("opacity-0");
         document.querySelector(".optelsom-container").classList.remove("opacity-0");
-        console.log(viewportHeight * 3.5);
+        console.log(viewportHeight * 3.3);
     }
     else if (scroll >= viewportHeight * 3) {
-        document.querySelector("#riot-img").src = "assets/img/riot3.jpg";
+        // document.querySelector("#riot-img").src = "assets/img/riot3.jpg";
+        document.querySelector(".riot").classList.remove("opacity-0");
         console.log(viewportHeight * 3);
     }
     else if (scroll >= viewportHeight * 2.8) {
-        document.querySelector("#riot-img").src = "assets/img/riot2.jpg";
+        // document.querySelector("#riot-img").src = "assets/img/riot2.jpg";
         console.log(viewportHeight * 2.8);
     }
     else if (scroll >= viewportHeight * 2.6) {
         document.querySelector(".riot").classList.remove("opacity-0");
-        document.querySelector("#riot-img").src = "assets/img/riot.jpg";
+        // document.querySelector("#riot-img").src = "assets/img/riot.jpg";
         console.log(viewportHeight * 2.6);
     }
     else if (scroll >= viewportHeight * 2.4) {
@@ -49,23 +49,55 @@ function scrollAnimation() {
         document.getElementById('article-title').classList.remove("opacity-0");
         document.getElementById('article-title').classList.add("smoothUpTransition");
         document.querySelector('.victim-container').classList.add("opacity-0");
+        document.querySelector('.hamburger-menu').classList.remove("opacity-0");
         console.log(viewportHeight);
     }
     else {
         document.getElementById('article-title').classList.add("opacity-0");
         document.querySelector('.victim-container').classList.remove("opacity-0");
+        document.querySelector('.hamburger-menu').classList.add("opacity-0");
         console.log("nothing");
 
     }
 }
 
-window.onload = function() { document.getElementById("blm-logo").addEventListener("click", blmLogo);
+function riotImages() {
+    let BackgroundImg=["assets/img/riot.jpg",
+        "assets/img/riot2.jpg",
+        "assets/img/riot3.jpg"
+    ];
+    setInterval(changeImage, 5000);
+    function changeImage() {
+        let i = Math.floor((Math.random() * 3));
+        document.querySelector("#riot-img").src = BackgroundImg[i];
+    }
+};
+
+window.onload = function(){
+    document.getElementById("blm-logo").addEventListener("click", blmLogo);
+    document.getElementById("title").addEventListener("click", scrollTitle);
+    document.getElementById("floyd").addEventListener("click", scrollFloyd);
+    riotImages();
+};
+
 function blmLogo() {
     document.querySelector(".blm-text-1").classList.add("visible-text");
     document.querySelector(".blm-text-2").classList.add("visible-text");
     document.querySelector(".blm-text-3").classList.add("visible-text");
     document.querySelector(".blm-text-4").classList.add("visible-text");
     console.log('click');
-}};
+}
+const scrollPosition = window.innerHeight;
 
+function scrollTitle() {
+    window.scrollTo({top: scrollPosition, behavior: 'smooth'});
+    document.getElementById("checkbox").checked = false;
+    console.log(scrollPosition);
+}
+
+function scrollFloyd() {
+    window.scrollTo({top: scrollPosition * 2, behavior: 'smooth'});
+    document.getElementById("checkbox").checked = false;
+    console.log(scrollPosition);
+}
 
